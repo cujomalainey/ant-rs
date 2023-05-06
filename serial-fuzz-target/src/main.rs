c5304b0f38c245bd211d858ee2307fdc408b1d71
Curtis Malainey <curtis@malainey.com>
2023-05-06 00:35:22 -0700

    driver: add fuzzer
    found a decent amount of array slicing bugs with it

---
+++ serial-fuzz-target/src/main.rs
@@ -0,0 +1,41 @@
+#[macro_use]
+extern crate afl;
+extern crate ant;
+use embedded_hal::serial::{Read, Write};
+use ant::drivers::{SerialDriver, Driver};
+
+struct SerialMock {
+    data: Vec<u8>,
+}
+
+impl Write<u8> for SerialMock {
+    type Error = u8;
+    fn write(&mut self, _word: u8) -> nb::Result<(), Self::Error> {
+        Ok(())
+    }
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+impl Read<u8> for SerialMock {
+    type Error = u8;
+    fn read(&mut self) -> nb::Result<u8, Self::Error> {
+        if self.data.len() > 0 {
+            Ok(self.data.remove(0))
+        } else {
+            Err(nb::Error::WouldBlock)
+        }
+    }
+}
+
+fn main() {
+    fuzz!(|data: &[u8]| {
+        let mut buf = Vec::new();
+        buf.extend_from_slice(data);
+        let mock = SerialMock { data: buf };
+        let mut driver: SerialDriver<SerialMock, ant::drivers::StubPin> = ant::drivers::SerialDriver::new(mock, None);
+        while driver.get_message() != Ok(None) {}
+    });
+}
+
