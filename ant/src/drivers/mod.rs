3ed8f0c3309b90ae723c30be97cbcf7673e1f654
Benjamin Tamasi <halftome@gmail.com>
2024-09-27 13:43:36 +0200

    make parse_buffer public

--- ant/src/drivers/mod.rs
+++ ant/src/drivers/mod.rs
@@ -138,7 +138,7 @@ const HEADER_SIZE: usize = 3;
 
 type Buffer = ArrayVec<u8, ANT_MESSAGE_SIZE>;
 
-fn parse_buffer<E>(buf: &[u8]) -> Result<Option<AntMessage>, DriverError<E>> {
+pub fn parse_buffer<E>(buf: &[u8]) -> Result<Option<AntMessage>, DriverError<E>> {
     // Not enough bytes
     if buf.len() < HEADER_SIZE {
         return Ok(None);
